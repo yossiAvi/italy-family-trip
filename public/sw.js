305c1d337a71036832f2b65cc4cf4a1e4dc2c78c
Yossi Avitan <yossi.avitan@gmail.com>
2026-07-23 13:25:32 +0300

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,18 @@
-const CACHE='avitan-trip-v1.5';
+const CACHE='avitan-trip-v2.1.2';
 const CORE=['/','/index.html','/manifest.webmanifest','/icon.svg','/avitan-family-hero.jpg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET')return;
-  event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{const copy=response.clone();if(new URL(event.request.url).origin===location.origin)caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('/index.html'))));
+  const url=new URL(event.request.url);
+  const isAppAsset=url.origin===location.origin && (event.request.mode==='navigate' || /\.(?:css|js|html)$/.test(url.pathname));
+  if(isAppAsset){
+    event.respondWith(fetch(event.request).then(response=>{
+      const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;
+    }).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('/index.html'))));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{
+    const copy=response.clone();if(url.origin===location.origin)caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;
+  }).catch(()=>caches.match('/index.html'))));
 });
